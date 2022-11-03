3f610dc31ab32a1a5e48a3c6745563fc84cf6ea8
RohanSoni-02 <soni.rohanchd@gmail.com>
2022-11-04 05:35:48 +1100

    Added animation on splash screen

--- app/src/main/java/com/example/customchatbot/spscreen.kt
+++ app/src/main/java/com/example/customchatbot/spscreen.kt
@@ -4,16 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.airbnb.lottie.LottieAnimationView
 
 class spscreen : AppCompatActivity() {
+    lateinit var lottieanimationview: LottieAnimationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spscreen)
+        lottieanimationview = findViewById(R.id.lottie)
+        lottieanimationview.playAnimation()
 
         Handler().postDelayed({
             val intent = Intent(this,login::class.java)
             startActivity(intent)
             finish()
-        },1000)
+        },9000)
     }
 }
